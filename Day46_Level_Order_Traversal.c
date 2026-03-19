c671a3f7a045e423b23ed000c654e7a277e3c687
khushanshuparihar <khushanshuparihar@gmail.com>
2026-03-19 12:00:00 +0530

    Add solution for Day 46

---
+++ Day46_Level_Order_Traversal.c
@@ -0,0 +1,25 @@
+/*
+Day 46: Level Order Traversal
+Problem: Level Order Traversal
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Level Order Traversal implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
