37f56b016bbb81559a297d0289a11019830e0bbe
khushanshuparihar <khushanshuparihar@gmail.com>
2026-03-21 12:00:00 +0530

    Add solution for Day 48

---
+++ Day48_Count_Leaf_Nodes.c
@@ -0,0 +1,25 @@
+/*
+Day 48: Count Leaf Nodes
+Problem: Count Leaf Nodes
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Count Leaf Nodes implementation
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    // Implementation here
+    printf("Result\n");
+    return 0;
+}
