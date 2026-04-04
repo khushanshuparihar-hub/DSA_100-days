a446aaa27399a5a3ee29422ac161b755f21bb9e4
khushanshuparihar <khushanshuparihar@gmail.com>
2026-04-04 12:00:00 +0530

    Add solution for Day 62

---
+++ Day62_Graph_Representation_Using_Adjacency_List.c
@@ -0,0 +1,29 @@
+/*
+Day 62: Graph Representation Using Adjacency List
+Problem: Build a graph with adjacency list representation. Use linked lists or dynamic arrays.
+
+Input:
+- n (vertices)
+- m (edges)
+- edges (u, v)
+
+Output:
+- List of adjacency lists for each vertex
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> adj[n + 1];
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); // For undirected graph
+    }
+    return 0;
+}
