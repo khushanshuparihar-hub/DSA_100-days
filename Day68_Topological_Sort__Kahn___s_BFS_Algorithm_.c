e21876cbdaed8649a727bd6d97424a81b39e8d38
khushanshuparihar <khushanshuparihar@gmail.com>
2026-04-10 12:00:00 +0530

    Add solution for Day 68

---
+++ Day68_Topological_Sort__Kahn___s_BFS_Algorithm_.c
@@ -0,0 +1,48 @@
+/*
+Day 68: Topological Sort (Kahnâ€™s BFS Algorithm)
+Problem: Implement topological sorting using in-degree array and queue (Kahnâ€™s Algorithm).
+*/
+
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> topoSort(int V, vector<int> adj[]) {
+    int indegree[V] = {0};
+    for(int i = 0; i < V; i++) {
+        for(auto it : adj[i]) {
+            indegree[it]++;
+        }
+    }
+    queue<int> q;
+    for(int i = 0; i < V; i++) {
+        if(indegree[i] == 0) {
+            q.push(i);
+        }
+    }
+    vector<int> topo;
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        topo.push_back(node);
+        for(auto it : adj[node]) {
+            indegree[it]--;
+            if(indegree[it] == 0) q.push(it);
+        }
+    }
+    return topo;
+}
+int main() {
+    int V, E;
+    cin >> V >> E;
+    vector<int> adj[V];
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+    }
+    vector<int> res = topoSort(V, adj);
+    for (int i : res) cout << i << " ";
+    return 0;
+}
