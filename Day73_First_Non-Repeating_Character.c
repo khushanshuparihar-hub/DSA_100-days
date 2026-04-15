a310e2fdedc4a1cd22bdae3e88b113f609d0848a
khushanshuparihar <khushanshuparihar@gmail.com>
2026-04-15 12:00:00 +0530

    Add solution for Day 73

---
+++ Day73_First_Non-Repeating_Character.c
@@ -0,0 +1,48 @@
+/*
+Day 73: First Non-Repeating Character
+Problem Statement
+Given a string s consisting of lowercase English letters, find and return the first character that does not repeat in the string. If all characters repeat, return '$'.
+
+Input Format
+A single string s.
+
+Output Format
+Print the first non-repeating character or '$' if none exists.
+
+Sample Input
+geeksforgeeks
+
+Sample Output
+f
+
+Explanation
+The character 'f' occurs only once in the string and appears before any other non-repeating character.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[1000];
+    int freq[26] = {0};
+    int found = 0;
+
+    scanf("%s", s);
+
+    for(int i = 0; s[i] != '\0'; i++) {
+        freq[s[i] - 'a']++;
+    }
+
+    for(int i = 0; s[i] != '\0'; i++) {
+        if(freq[s[i] - 'a'] == 1) {
+            printf("%c", s[i]);
+            found = 1;
+            break;
+        }
+    }
+
+    if(!found)
+        printf("$");
+
+    return 0;
+}
